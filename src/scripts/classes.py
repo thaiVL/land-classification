ef8259a363cf84a0c023a7c94898b6d31d2e7e50
Thai <thaila71669@gmail.com>
2023-11-10 22:57:06 -0600

    Moved classes out of notebook and into python scripts.
    NOTE: Ran into import issues regarding importing from a sibling package,
    so decided to leave notebooks under the src directory to avoid import
    errors.

---
+++ src/scripts/classes.py
@@ -0,0 +1,65 @@
+from enum import Enum
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+
+class Label(Enum):
+  ANNUALCROP = 0
+  FOREST = 1
+  HERBACEOUSVEGETATIOM = 2
+  HIGHWAY = 3
+  INDUSTRIAL = 4
+  PASTURE = 5
+  PERMANENTCROP = 6
+  RESIDENTIAL = 7
+  RIVER = 8
+  SEALAKE = 9
+
+  __labels = {
+    ANNUALCROP: "AnnualCrop",
+    FOREST: "Forest",
+    HERBACEOUSVEGETATIOM: "HerbaceousVegetation",
+    HIGHWAY: "Highway",
+    INDUSTRIAL: "Industrial",
+    PASTURE: "Pasture",
+    PERMANENTCROP: "PermanentCrop",
+    RESIDENTIAL: "Residential",
+    RIVER: "River",
+    SEALAKE: "SeaLake"
+  }
+
+  @classmethod
+  def string_label(cls, label: int):
+    return cls.__labels[label]
+
+class Sample:
+  __img_name: str
+  __img_array: npt.NDArray[np.float64]
+  __img_label: Label
+
+  @property
+  def img_name(self):
+    return self.__img_name
+
+  @property
+  def img_array(self):
+    return self.__img_array
+
+  @property
+  def img_label(self):
+    return self.__img_label
+
+  def __init__(self, img_name: str, img_array: npt.NDArray[np.float64], img_label: Label) -> None:
+    self.__img_name = img_name
+    self.__img_array = img_array
+    self.__img_label = img_label
+
+  @classmethod
+  def reading_list(cls, df: pd.DataFrame)->list:
+    return list(map(lambda x:Sample(img_name=x[0], img_array=x[2], img_label=x[1]), df.values.tolist()))
+
+  def __str__(self) -> str:
+    return f"Image name: {self.__img_name} | Label: {self.__img_label}"
+
+  def __repr__(self) -> str:
+    return self.__str__()
