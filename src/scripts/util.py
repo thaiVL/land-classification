4f80473a1a4fdf4b145972b28f890780af33a513
Thai <thaila71669@gmail.com>
2023-11-14 23:03:00 -0600

    WIP Working on training model, need to freeze the CNN layers and configure the dense layers

--- src/scripts/util.py
+++ src/scripts/util.py
@@ -1,5 +1,10 @@
 import skimage.util as skutil
 import skimage.io as skio
+import tensorflow as tf
 
 def read_img(path):
-  return skutil.img_as_float64(skio.imread(path))
+  return tf.convert_to_tensor(skutil.img_as_float64(skio.imread(path)), dtype=tf.float64)
+
+def tf_read_img(path):
+  return tf.convert_to_tensor(tf.keras.utils.load_img(path=path, grayscale=False, color_mode="rgb"), dtype=tf.float64)
+  
