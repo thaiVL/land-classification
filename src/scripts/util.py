14b24d72100b4f14d39a41b8fe17e245459d7532
Thai <thaila71669@gmail.com>
2023-11-20 23:18:36 -0600

    Updated gitignore and modified model to use GPU hardware acceleration

--- src/scripts/util.py
+++ src/scripts/util.py
@@ -7,11 +7,16 @@ import pandas as pd
 
 
 def read_img(path: str) -> npt.NDArray[np.float64]:
-  return skutil.img_as_float64(skio.imread(path))
+  return tf.convert_to_tensor(skutil.img_as_float64(skio.imread(path)), dtype=tf.float64)
 
 def pd_series_to_np_img(series: pd.Series) -> npt.NDArray[np.float64]:
   vanilla_list = list(series.values)
   return np.array(vanilla_list, dtype=np.float64)
+
+def pd_series_to_tensor(series: pd.Series) -> tf.Tensor:
+    vanilla_list = list(series.values)
+    return tf.convert_to_tensor(vanilla_list, dtype=tf.float64)
+
 
 def tf_read_img(path):
   return tf.convert_to_tensor(tf.keras.utils.load_img(path=path, grayscale=False, color_mode="rgb"), dtype=tf.float64)
